6f3df73e9dc04b3b9835625eaeefa5ab8dbd3e80
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2025-09-22 18:13:38 +0900

    Add base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://yharuto0917.github.io/POS-Register-System-for-Soshosai-Manuals",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
